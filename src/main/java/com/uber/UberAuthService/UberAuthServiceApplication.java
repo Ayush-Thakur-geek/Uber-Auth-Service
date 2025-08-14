73f8844cfd6bd3aed786192dd34380a85d38e63e
Ayush Thakur <ayush26819@gmail.com>
2025-08-14 22:52:30 +0530

    Initial setup

---
+++ src/main/java/com/uber/UberAuthService/UberAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.uber.UberAuthService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UberAuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UberAuthServiceApplication.class, args);
+	}
+
+}
